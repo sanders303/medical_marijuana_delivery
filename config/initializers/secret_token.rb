18f0b59a788f24b86d4aec1082f0eaaffb51dc66
Prelang Builder <builder@prelang.com>
2014-04-15 15:24:19 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedicalMarijuanaDelivery::Application.config.secret_key_base = '1320e44476b64167a27d3c5a3489908d2356fa8d8776c1dd4ec1ff14dbd4fb4f65a2cda91c80556506456a2b5bd0242aa8791cc640603e7ca5a26b4f480c2d04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedicalMarijuanaDelivery::Application.config.session_store :cookie_store, key: '_medical_marijuana_delivery_session'
