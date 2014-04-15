6e404e00da32f952471887836b1b09a14a056e22
Prelang Builder <builder@prelang.com>
2014-04-15 15:26:29 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MedicalMarijuanaDelivery::Application.routes.draw do
+  get "landings/index"
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
